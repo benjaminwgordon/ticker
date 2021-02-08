968d2b33e866e248f9acc73ced14ec4cdd0e993d
Ben Gordon <Bengordon1995@gmail.com>
2021-02-08 14:47:20 -0600

    working timescale selector

--- src/StockPlot/StockPlotControls.jsx
+++ src/StockPlot/StockPlotControls.jsx
@@ -21,13 +21,13 @@ const StockPlotControls = (props) => {
             <div>
                 <button type="submit">Submit</button>
                 <label htmlFor="1d-radio">1d</label>
-                <input type="radio" name="timescale" value="1d" id="1d-radio" />
+                <input type="radio" name="timescale" value="1d" id="1d-radio" checked={timeScale === "1d"} onChange={() => setTimescale("1d")}/>
                 <label htmlFor="5d-radio">5d</label>
-                <input type="radio" name="timescale" value="5d" id="5d-radio" />
+                <input type="radio" name="timescale" value="5d" id="5d-radio" checked={timeScale === "5d"} onChange={() => setTimescale("5d")}/>
                 <label htmlFor="1m-radio">1m</label>
-                <input type="radio" name="timescale" value="1m" id="1m-radio" />
+                <input type="radio" name="timescale" value="1m" id="1m-radio" checked={timeScale === "1m"} onChange={() => setTimescale("1m")}/>
                 <label htmlFor="6m-radio">6m</label>
-                <input type="radio" name="timescale" value="6m" id="6m-radio" />
+                <input type="radio" name="timescale" value="6m" id="6m-radio" checked={timeScale === "6m"} onChange={() => setTimescale("6m")}/>
             </div>
         </div>
     )
